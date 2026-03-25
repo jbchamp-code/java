80a051cbdd7e90bba7d5d5d1ce2ee5e1c214c776
jbchamp-code <jbclansangan@gmail.com>
2026-03-25 07:48:55 +0000

    chapter 2 tasks

---
+++ CIICC/task/Main.java
@@ -0,0 +1,11 @@
+package CIICC.task;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        task2 task = new task2();
+        task.printing();
+
+        
+    }
+}
---
+++ CIICC/task/task2.java
@@ -0,0 +1,20 @@
+package CIICC.task;
+public class task2{
+
+    public void printing(){
+        char letter_H = 'H', letter_w = 'w', letter_r = 'r', letter_d = 'd';
+        byte zero = 0;
+        short wan = 1;
+        int triwanwanzero = 3110;
+        float tudotzero = 2.0f;
+        boolean tru = true;
+            
+        String output = letter_H + "" + triwanwanzero + " " ;
+        output += letter_w + "" + zero + "" + letter_r + "" + wan + letter_d + " ";
+        output += tudotzero + " " + tru;
+            
+        System.out.println(output);
+    }
+    
+        
+}
