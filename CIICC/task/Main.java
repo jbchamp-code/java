11b7e4d2efe196f33c642990345b6607e6669a0f
jbchamp-code <jbclansangan@gmail.com>
2026-04-08 01:13:34 +0000

    up

--- CIICC/task/Main.java
+++ CIICC/task/Main.java
@@ -10,11 +10,12 @@ public class Main {
         //labActivity act = new labActivity();
         //act.highestnumb();
         
-        task3 t3 = new task3();
-        t3.changevariables();
+        //task3 t3 = new task3();
+        //t3.changevariables();
 
         //task4 t4 = new task4();
         //t4.palindrome();
         
+        System.out.println("Latest task4. please check main java.");
     }
 }
